7decb2f25aa1b1b83155a773d857acf1b1d52097
nntram <tramb9531@gmail.com>
2025-07-12 22:09:41 +0700

    Add git

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using WPFExample1.Views;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow1 ex1 = new MainWindow1();
+            this.Hide();
+
+            // When second window closes, show this window again
+            ex1.Closed += (s, args) => this.Show();
+
+            ex1.Show();   
+        }
+    }
+}
---
+++ WPFExample1/Views/MainWindow1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+
+
+namespace WPFExample1.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow1.xaml
+    /// </summary>
+    public partial class MainWindow1 : Window
+    {
+        public MainWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanelExample ex1 = new StackPanelExample();
+            this.Hide();
+
+            // When second window closes, show this window again
+            ex1.Closed += (s, args) => this.Show();
+
+            ex1.Show();
+        }
+
+        private void ButtonGrid_Click(object sender, RoutedEventArgs e)
+        {
+            GridExample ex2 = new GridExample();
+            this.Hide();
+
+            // When second window closes, show this window again
+            ex2.Closed += (s, args) => this.Show();
+
+            ex2.Show();
+        }
+
+        private void ButtonWrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+            WrapPanelExample ex3 = new WrapPanelExample();
+            this.Hide();
+
+            // When second window closes, show this window again
+            ex3.Closed += (s, args) => this.Show();
+
+            ex3.Show();
+        }
+    }
+}
